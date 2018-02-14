db23c1d139bbe9941cd51121e2c8ba2bf13f9e6d
godkim12 <sakkarinsuraban@gmail.com>
2018-02-14 11:49:12 +0700

    login fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,5 +35,23 @@ public class Controller implements Initializable{
         }
 
     }//intitiallze
+@FXML
+    public  void Login(ActionEvent event){
+        System.out.println(username.getText());
+        System.out.println(password.getText());
+
+        try {
+            if (this.model.isLogin(username.getText(),password.getText())){
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+               // stage.close();
+                loginStatus.setText("Welcome to Our System");
+            }else {
+                loginStatus.setText("Your username or password is invlid");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+}//Login
+
 
 } //class
